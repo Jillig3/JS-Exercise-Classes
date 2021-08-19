19fd79dfa5274193e4c030a0b3c149ac0a50d1d1
unknown <jacobillig@gmail.com>
2021-08-19 17:27:11 -0500

    Completed all Tasks

--- index.js
+++ index.js
@@ -211,14 +211,21 @@ class ProjectManager extends Instructor{
   super(attrs);
   this.gradClassName = attrs.gradClassName;
   this.favInstructor = attrs.favInstructor;
+  this.channel = attrs.channel;
   } 
   standUp(){
-    return `${this.name} announces to ${channel}, @channel standy times!`
+    return `${this.name} announces to eu3, @channel standy times!`
   }
-  debugsCode(){
+  debugsCode(student, subject){
     return `${this.name} debugs ${student.name}'s code on ${subject}`
   }
 }
+const channel = new ProjectManager({
+  name: 'jake',
+  channel: 'web 47',
+  subject: 'JavaScript',
+
+});
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
